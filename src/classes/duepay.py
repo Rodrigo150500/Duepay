cdd3f264dd648db8e70fb2db802d165ebb1830ac
Rodrigo Takara <rodrigo.takara1505@gmail.com>
2025-01-24 18:25:56 -0300

    Progress: duepay class

--- src/classes/duepay.py
+++ src/classes/duepay.py
@@ -1,13 +1,22 @@
 import os
+import pandas as pd
 from src.utils.path_finder import path_finder
 
 class Duepay:
 
     def extract_total(self):
-        folder_path = path_finder(f"../output")
-        print()
-        print(folder_path)
+        folder_path = path_finder(f"../input")
 
-obj = Duepay()
-obj.extract_total()
+        file_in_folder = os.listdir(folder_path)
+
+        file_path = os.path.join(folder_path, file_in_folder[0])
+        
+        duepay_excel = pd.read_csv(file_path)
+
+        print()
+        print(duepay_excel)
+
+        
+
+
 
