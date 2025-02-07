5e85565524aabba0731a0a322159fd343b4528d3
Rodrigo Takara <rodrigo.takara1505@gmail.com>
2025-02-07 17:50:51 -0300

    Feat: Implementing unzip function

--- src/utils/unzip_vendas.py
+++ src/utils/unzip_vendas.py
@@ -1,23 +1,25 @@
-import os
+from os import path, listdir
 import zipfile
 from src.utils.path_finder import path_finder
+from src.utils.clean_zip_file import clean_zip_file
 
 def unzip_file(origin: str) -> None:
 
-    path_folder_origin = path_finder(f"../{origin}")
+  #Encontrando a pasta do arquivo zip
+  path_folder_origin = path_finder(f"../{origin}")
 
+  #Capturando apenas o nome do arquivo
+  zipFileName = listdir(path_folder_origin)[0]
 
-    zipFileName = os.listdir(path_folder_origin)[0]
+  #Juntando o diretório da pasta + nome do arquivo
+  zipFilePath = path.join(path_folder_origin, zipFileName)
 
-    zipFilePath = os.path.join(path_folder_origin, zipFileName)
+  if path.isdir(path_folder_origin) and path.exists(zipFilePath):
+      
+    with zipfile.ZipFile(zipFilePath, "r") as zip_ref:
+      zip_ref.extractall(path=path_folder_origin)
 
-    if os.path.isdir(path_folder_origin) and os.path.exists(zipFilePath):
-        
-      with zipfile.ZipFile(zipFilePath, "r") as zip_ref:
-        zip_ref.extract(path_folder_origin)
-    else:
-       print("Caminho não encontrado")
+    clean_zip_file(path_folder_origin)
 
-
-
-unzip_file("input/vendas")
+  else:
+     print("Caminho não encontrado ou localização errada")
