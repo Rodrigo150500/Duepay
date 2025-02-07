1c3168e83efe6da853a410cf39bf9325147e5a96
Rodrigo Takara <rodrigo.takara1505@gmail.com>
2025-02-07 16:38:09 -0300

    Delete: Change file name

---
+++ src/utils/clean_zip_file.py
@@ -0,0 +1,16 @@
+import os
+from src.utils.path_finder import path_finder
+
+def clean_zip_file(path: str) -> None:
+    path_folder = path_finder(path)
+
+    files = os.listdir(path_folder)
+
+    if os.path.isdir(path_folder) and os.path.exists(path_folder):
+
+        for file in files:
+            
+            file_path = os.path.join(path_folder, file)
+
+            if os.path.isfile(file_path) and ".zip" in file:
+                os.remove(file_path)
