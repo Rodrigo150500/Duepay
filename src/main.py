559d81a54e1aba2016e2234b463cb6625194eec1
Rodrigo Takara <rodrigo.takara1505@gmail.com>
2025-02-13 10:38:36 -0300

    Add: Adding unzip function to main

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
+from src.utils.unzip_vendas import unzip_file
 from src.classes.dataframe_merged import DataframeMerged
 
-
 def main():
+
+    unzip_file('input/vendas')
+
     dataframe = DataframeMerged()
     dataframe.dataframe_merged()
 
