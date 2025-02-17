a3e5657372b08f31d7a7fc97bb99a9ae67c7b4b5
Rodrigo Takara <rodrigo.takara1505@gmail.com>
2025-02-17 09:16:36 -0300

    Progress: Commiting utils folder

--- src/utils/validate_input_choice.py
+++ src/utils/validate_input_choice.py
@@ -1,11 +1,12 @@
 def validate_input_choice(msg: str) -> int:
+    while True:
+        try:
+            escolha = int(input(msg))
+            
+            if 1 <= escolha <= 3:  # Verifica se está no intervalo válido
+                return escolha
+            else:
+                print("\nDigite um valor válido (1, 2 ou 3).")
 
-    escolha = int(input(msg))
-
-    while escolha <= 0 or escolha >= 4:
-        print("\nDigite um valor válido")
-        
-        escolha = int(input(msg))
-
-    return escolha
-
+        except ValueError:  # Captura erro de conversão
+            print("\nEntrada inválida! Digite um número válido.")
